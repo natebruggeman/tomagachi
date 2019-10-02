8070c3d3545e7eff020e41c0e9a75e55a14746b0
Nate Bruggeman <you@example.com>
2019-10-02 10:37:02 -0500

    Timer set up

--- js/app.js
+++ js/app.js
@@ -1,17 +1,43 @@
-class Tomagatchi{
-	constructor(hunger, sleepiness, boredom, age){
-	this.hunger = hunger;
-	this.sleepiness = sleepiness;
-	this.boredom = boredom;
-	this.age = age;
-	}
-	giveStatus(levels){
-		console.log(`Hello it's me, your Tomagatchi, My hunger level is ${this.hunger}, my sleepiness is ${this.sleepiness}, I am this ${this.boredom} bored. I'm growing up quick, I am ${this.age} days old!`);
+$('#start').on('click', () =>{
+	console.log('start button works');
+	game.setTimer();
+});
+
+
+
+// class Tomagatchi{
+// 	constructor(hunger, sleepiness, boredom, age){
+// 	this.hunger = hunger;
+// 	this.sleepiness = sleepiness;
+// 	this.boredom = boredom;
+// 	this.age = age;
+// 	}
+// 	giveStatus(levels){
+// 		console.log(`Hello it's me, your Tomagatchi, My hunger level is ${this.hunger}, my sleepiness is ${this.sleepiness}, I am this ${this.boredom} bored. I'm growing up quick, I am ${this.age} days old!`);
+// 	}
+// }
+
+// const tommy = new Tomagatchi('0', '0', '0', '0')
+// tommy.giveStatus()
+
+
+const game = {
+	time: 0,
+	setTimer(){
+
+		const $timer = $('#timer');
+
+		const interval = setInterval(() => {
+
+			if(this.time === 100){
+				clearInterval(interval)
+				} else {
+				this.time++
+			}
+			$timer.text(`Days old: ${this.time}`)
+
+		}, 1000)
 	}
 }
-
-const tommy = new Tomagatchi('0', '0', '0', '0')
-tommy.giveStatus()
-
 
 
